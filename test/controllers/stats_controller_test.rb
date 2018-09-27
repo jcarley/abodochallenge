3e626cea79316e38762463e96026e58e14665e00
jcarley <jeff.carley@gmail.com>
2018-09-27 07:54:54 -0500

    Updated to fix a broken test

--- test/controllers/stats_controller_test.rb
+++ test/controllers/stats_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class StatsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get stats_index_url
+    get stats_url
     assert_response :success
   end
 
